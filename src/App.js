11d3401b3e6fcb50ba1fb000f0cf94d3e97f303d
henrique <engs-henriquezaluk@camporeal.edu.br>
2024-08-05 21:14:42 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import './assets/estilo.css';
+
+function App(){
+  const [nutri, setNutri] = useState([]);
+
+
+  useEffect(()=>{
+    let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+
+    fetch(url)    
+    .then((r) => r.json())  
+    .then((json) => {
+      setNutri(json);
+
+    })
+  }, []);
+
+  return(
+    <div>
+      <header>
+        <strong>React Nutri</strong>
+      </header>
+
+      {nutri.map((item)=>{
+        return(
+          <article key={item.id}>
+            <strong>{item.titulo}</strong>
+            <img src={item.capa} alt={item.titulo}/>
+            <p>{item.subtitulo}</p>
+            <span>{item.categoria}</span>
+          </article>
+        );
+      })
+      }
+
+    </div>
+
+  );
+}
+
+export default App;
